590f2042041bee5366156c6334af2d942694a6a3
eunsujang3028 <wkddmstn3020@gamil.com>
2025-10-23 11:25:51 +0900

    login

--- models/users.py
+++ models/users.py
@@ -32,26 +32,37 @@ class DriverLicenseType(enum.Enum):
     YES = "YES"
     NO = "NO"
 
-
-
-# 유저 생성용
-class UserCreate(BaseModel):
+# 로그인 요청 받을 때 쓸 모델
+class UserLogin(BaseModel):
     nickname: str
     password: str
-    user_name: str
-    gender: Optional[GenderType] = None
-    age: Optional[int] = None
-    job: Optional[JobType] = None
-    drinking: Optional[DrinkingType] = None
-    smoking: Optional[SmokingType] = None
-    drive_license: Optional[DriverLicenseType] = None
 
-
-# 응답용
-class User(UserCreate):
+# 로그인 성공 시 클라이언트로 돌려줄 데이터
+class UserResponse(BaseModel):
     id: int
-    created_at: Optional[str] = None
-    updated_at: Optional[str] = None
+    nickname: str
+    user_name: str
 
-    class Config:
-        orm_mode = True
+# ---------------------------------------------------
+# # 유저 생성용
+# class UserCreate(BaseModel):
+#     nickname: str
+#     password: str
+#     user_name: str
+#     gender: Optional[GenderType] = None
+#     age: Optional[int] = None
+#     job: Optional[JobType] = None
+#     drinking: Optional[DrinkingType] = None
+#     smoking: Optional[SmokingType] = None
+#     drive_license: Optional[DriverLicenseType] = None
+
+
+# # 응답용
+# class User(UserCreate):
+#     id: int
+#     created_at: Optional[str] = None
+#     updated_at: Optional[str] = None
+
+#     class Config:
+#         orm_mode = True
+
